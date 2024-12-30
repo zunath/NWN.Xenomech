367b28a4993a5b30c0e6ba9373e507e6133d7a20
Zunath <coolty3001@yahoo.com>
2024-12-30 13:13:28 -0500

    Area cache service

---
+++ XM.Area/AreaCacheService.cs
@@ -0,0 +1,53 @@
+﻿using Anvil.Services;
+using NLog;
+using System.Collections.Generic;
+using XM.Core.EventManagement.XMEvent;
+
+namespace XM.Area
+{
+    [ServiceBinding(typeof(AreaCacheService))]
+    [ServiceBinding(typeof(IXMOnCacheDataBefore))]
+    public class AreaCacheService: IXMOnCacheDataBefore
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        private readonly Dictionary<string, uint> _areasByResref;
+
+        public AreaCacheService()
+        {
+            _areasByResref = new Dictionary<string, uint>();
+        }
+
+        /// <summary>
+        /// Retrieves an area by its resref. If the area does not exist, OBJECT_INVALID will be returned.
+        /// </summary>
+        /// <param name="resref">The resref to use for the search.</param>
+        /// <returns>The area ID or OBJECT_INVALID if area does not exist.</returns>
+        public uint GetAreaByResref(string resref)
+        {
+            if (!_areasByResref.ContainsKey(resref))
+                return OBJECT_INVALID;
+
+            return _areasByResref[resref];
+        }
+
+        public void OnCacheDataBefore()
+        {
+            CacheAreasByResref();
+        }
+
+        /// <summary>
+        /// Caches all areas by their resref.
+        /// </summary>
+        private void CacheAreasByResref()
+        {
+            for (var area = GetFirstArea(); GetIsObjectValid(area); area = GetNextArea())
+            {
+                var resref = GetResRef(area);
+                _areasByResref[resref] = area;
+            }
+
+            _logger.Info($"Cached {_areasByResref.Count} areas by resref.");
+        }
+    }
+}
