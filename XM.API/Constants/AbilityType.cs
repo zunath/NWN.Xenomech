f7da3c205f709ef56ac2d527a245b2c6b3cdd8e9
Zunath <coolty3001@yahoo.com>
2024-12-29 19:12:40 -0500

    Rename ability types

--- XM.API/Constants/AbilityType.cs
+++ XM.API/Constants/AbilityType.cs
@@ -4,11 +4,11 @@ namespace XM.API.Constants
     public enum AbilityType
     {
         Invalid = -1,
-        Strength = 0,
-        Dexterity = 1,
-        Constitution = 2,
-        Intelligence = 3,
-        Wisdom = 4,
-        Charisma = 5
+        Might = 0,
+        Perception = 1,
+        Vitality = 2,
+        Agility = 3,
+        Willpower = 4,
+        Social = 5
     }
 }
