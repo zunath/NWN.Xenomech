dce1df7d1608bb1a687ba20fb583ff0f3c194bfd
Zunath <coolty3001@yahoo.com>
2024-12-30 22:58:14 -0500

    Recast service

---
+++ XM.Combat/Entity/PlayerCombat.cs
@@ -0,0 +1,25 @@
+﻿using Anvil.Services;
+using System.Collections.Generic;
+using System;
+using XM.Combat.Recast;
+using XM.Data;
+
+namespace XM.Combat.Entity
+{
+    [ServiceBinding(typeof(IDBEntity))]
+    internal class PlayerCombat: EntityBase
+    {
+        public PlayerCombat()
+        {
+            RecastTimes = new Dictionary<RecastGroup, DateTime>();
+        }
+
+        public PlayerCombat(string id)
+        {
+            Id = id;
+        }
+
+        public Dictionary<RecastGroup, DateTime> RecastTimes { get; set; }
+        public int AbilityRecastReduction { get; set; }
+    }
+}
---
+++ XM.Combat/Recast/RecastGroupAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using XM.Localization;
+
+namespace XM.Combat.Recast
+{
+    public class RecastGroupAttribute : Attribute
+    {
+        public LocaleString Name { get; set; }
+        public LocaleString ShortName { get; set; }
+        public bool IsVisible { get; set; }
+
+        public RecastGroupAttribute(LocaleString name, LocaleString shortName, bool isVisible)
+        {
+            Name = name;
+            ShortName = shortName;
+            IsVisible = isVisible;
+        }
+    }
+}
