777ea4161e5f82369676ac2b42f703ed0aa5ba9f
Zunath <coolty3001@yahoo.com>
2024-12-30 19:43:14 -0500

    Add base migration so the version can be determined.

---
+++ XM.Migration/PlayerMigration/_1_BaseMigration.cs
@@ -0,0 +1,11 @@
+﻿namespace XM.Migration.PlayerMigration
+{
+    internal class _1_BaseMigration: IPlayerMigration
+    {
+        public int Version => 1;
+        public void Migrate(uint player)
+        {
+            
+        }
+    }
+}
