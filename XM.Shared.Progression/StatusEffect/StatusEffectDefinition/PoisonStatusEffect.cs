f48b3b6f9506f53d5aa8d7a54950e6befb258a84
Zunath <coolty3001@yahoo.com>
2025-02-15 19:28:49 -0500

    Poison status effect

---
+++ XM.Shared.Progression/StatusEffect/StatusEffectDefinition/PoisonStatusEffect.cs
@@ -0,0 +1,32 @@
+﻿using XM.Progression.Stat;
+using XM.Shared.API.Constants;
+using XM.Shared.Core.Localization;
+
+namespace XM.Progression.StatusEffect.StatusEffectDefinition
+{
+    internal class PoisonStatusEffect: StatusEffectBase
+    {
+        public override LocaleString Name => LocaleString.Poison;
+        public override EffectIconType Icon => EffectIconType.Poison;
+        public override StatusEffectStackType StackingType => StatusEffectStackType.StackFromMultipleSources;
+        public override float Frequency => 3f;
+
+        private readonly StatService _stat;
+        public PoisonStatusEffect(StatService stat)
+        {
+            _stat = stat;
+        }
+
+        protected override void Tick(uint creature)
+        {
+            var maxHP = _stat.GetMaxHP(creature);
+            var damage = (int)(maxHP * 0.01f);
+
+            if (damage < 1)
+                damage = 1;
+
+            ApplyEffectToObject(DurationType.Instant, EffectDamage(damage, DamageType.Acid), creature);
+            ApplyEffectToObject(DurationType.Instant, EffectVisualEffect(VisualEffectType.ImpPoisonSmall), creature);
+        }
+    }
+}
