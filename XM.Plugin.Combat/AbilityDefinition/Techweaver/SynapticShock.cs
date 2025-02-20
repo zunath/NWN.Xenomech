3ae506d7f8f3c737d0efddf8b0eb5d00ac21b8aa
Zunath <coolty3001@yahoo.com>
2025-02-19 21:26:15 -0500

    Synaptic Shock

---
+++ XM.Plugin.Combat/AbilityDefinition/Techweaver/SynapticShock.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Anvil.Services;
+using XM.AI.Enmity;
+using XM.Progression.Ability;
+using XM.Progression.Recast;
+using XM.Progression.Stat;
+using XM.Progression.StatusEffect;
+using XM.Shared.API.Constants;
+using XM.Shared.Core.Localization;
+using XM.Shared.Core.Party;
+
+namespace XM.Plugin.Combat.AbilityDefinition.Techweaver
+{
+    [ServiceBinding(typeof(IAbilityListDefinition))]
+    internal class SynapticShock : AbilityBase
+    {
+        private readonly AbilityBuilder _builder = new();
+        private readonly SpellService _spell;
+        private readonly EnmityService _enmity;
+
+        public SynapticShock(
+            PartyService party,
+            StatusEffectService status,
+            SpellService spell,
+            EnmityService enmity)
+            : base(party, status)
+        {
+            _spell = spell;
+            _enmity = enmity;
+        }
+
+        public override Dictionary<FeatType, AbilityDetail> BuildAbilities()
+        {
+            SynapticShockAbility();
+
+            return _builder.Build();
+        }
+
+        private void Impact(uint activator, uint target)
+        {
+            var ticks = _spell.CalculateResistedTicks(target, ResistType.Mind, 32);
+            if (ticks > 0)
+            {
+                var duration = ticks * 0.25f;
+                AssignCommand(activator, () => ApplyEffectToObject(DurationType.Temporary, EffectStunned(), target, duration)); 
+                AssignCommand(activator, () => ApplyEffectToObject(DurationType.Instant, EffectVisualEffect(VisualEffectType.ImpReduceAbilityScore), target));
+            }
+
+            _enmity.ModifyEnmity(activator, target, EnmityType.Volatile, 850);
+        }
+
+        private void SynapticShockAbility()
+        {
+            _builder.Create(FeatType.SynapticShock)
+                .Name(LocaleString.SynapticShock)
+                .Description(LocaleString.SynapticShockDescription)
+                .HasRecastDelay(RecastGroup.SynapticShock, 60f)
+                .IsCastedAbility()
+                .HasActivationDelay(1f)
+                .RequirementEP(12)
+                .ResonanceCost(1)
+                .HasMaxRange(12f)
+                .IsHostile()
+                .HasImpactAction((activator, target, location) =>
+                {
+                    Impact(activator, target);
+                });
+        }
+    }
+}
