483235e8d937ede4e636b1e27a5c475e25637c78
Zunath <coolty3001@yahoo.com>
2025-02-22 10:40:50 -0500

    Mercy Strike

---
+++ XM.Plugin.Combat/AbilityDefinition/Beastmaster/MercyStrike.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using Anvil.Services;
+using XM.Plugin.Combat.StatusEffectDefinition.Debuff;
+using XM.Progression.Ability;
+using XM.Progression.Recast;
+using XM.Progression.Stat;
+using XM.Progression.StatusEffect;
+using XM.Shared.API.Constants;
+using XM.Shared.Core.Localization;
+using XM.Shared.Core.Party;
+
+namespace XM.Plugin.Combat.AbilityDefinition.Beastmaster
+{
+    [ServiceBinding(typeof(IAbilityListDefinition))]
+    internal class MercyStrike: AbilityBase
+    {
+        private readonly AbilityBuilder _builder = new();
+        private readonly SpellService _spell;
+
+        public MercyStrike(
+            PartyService party,
+            StatusEffectService status,
+            SpellService spell)
+            : base(party, status)
+        {
+            _spell = spell;
+        }
+
+        private void Impact(uint activator, List<uint> targets, int dmg)
+        {
+            ApplyEffectToObject(DurationType.Instant, EffectVisualEffect(VisualEffectType.ImpDeathWard), activator);
+
+            foreach (var target in targets)
+            {
+                if (target == activator)
+                    continue;
+
+                var damage = _spell.CalculateSpellDamage(activator, target, dmg, ResistType.Earth, AbilityType.Might, AbilityType.Might);
+                AssignCommand(activator, () =>
+                {
+                    ApplyEffectToObject(DurationType.Instant, EffectDamage(damage, DamageType.Darkness), target);
+                });
+            }
+        }
+
+        public override Dictionary<FeatType, AbilityDetail> BuildAbilities()
+        {
+            MercyStrike1();
+            MercyStrike2();
+
+            return _builder.Build();
+        }
+
+        private void MercyStrike1()
+        {
+            _builder.Create(FeatType.MercyStrike1)
+                .Name(LocaleString.MercyStrikeI)
+                .Description(LocaleString.MercyStrikeIDescription)
+                .HasRecastDelay(RecastGroup.MercyStrike, 30f)
+                .HasActivationDelay(2f)
+                .DisplaysVisualEffectWhenActivating()
+                .IsCastedAbility()
+                .RequirementEP(18)
+                .ResonanceCost(1)
+                .TelegraphSize(4f, 4f)
+                .HasTelegraphSphereAction((activator, targets, targetLocation) =>
+                {
+                    Impact(activator, targets, 24);
+                });
+        }
+
+        private void MercyStrike2()
+        {
+            _builder.Create(FeatType.MercyStrike2)
+                .Name(LocaleString.MercyStrikeII)
+                .Description(LocaleString.MercyStrikeIIDescription)
+                .HasRecastDelay(RecastGroup.MercyStrike, 30f)
+                .HasActivationDelay(2f)
+                .DisplaysVisualEffectWhenActivating()
+                .IsCastedAbility()
+                .RequirementEP(34)
+                .ResonanceCost(2)
+                .TelegraphSize(4f, 4f)
+                .HasTelegraphSphereAction((activator, targets, targetLocation) =>
+                {
+                    Impact(activator, targets, 44);
+                });
+        }
+    }
+}
