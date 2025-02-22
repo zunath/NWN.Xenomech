94f44d36ca74c74c8e1f1d1703d23fad6609a8ea
Zunath <coolty3001@yahoo.com>
2025-02-21 20:13:58 -0500

    Flame

---
+++ XM.Plugin.Combat/AbilityDefinition/Elementalist/Flame.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using Anvil.API;
+using Anvil.Services;
+using XM.Progression.Ability;
+using XM.Progression.Recast;
+using XM.Progression.Stat;
+using XM.Progression.StatusEffect;
+using XM.Shared.API.Constants;
+using XM.Shared.Core.Localization;
+using XM.Shared.Core.Party;
+using DamageType = XM.Shared.API.Constants.DamageType;
+
+namespace XM.Plugin.Combat.AbilityDefinition.Elementalist
+{
+    [ServiceBinding(typeof(IAbilityListDefinition))]
+    internal class Flame: AbilityBase
+    {
+        private readonly AbilityBuilder _builder = new();
+        private readonly SpellService _spell;
+
+        public Flame(
+            PartyService party,
+            StatusEffectService status,
+            SpellService spell)
+            : base(party, status)
+        {
+            _spell = spell;
+        }
+
+        public override Dictionary<FeatType, AbilityDetail> BuildAbilities()
+        {
+            Flame1();
+            Flame2();
+            Flame3();
+
+            return _builder.Build();
+        }
+
+        private void Impact(uint activator, uint target, int dmg)
+        {
+            var damage = _spell.CalculateSpellDamage(activator, target, dmg, ResistType.Fire);
+            ApplyEffectToObject(DurationType.Instant, EffectVisualEffect(VisualEffectType.ImpFlameSmall), target);
+            AssignCommand(activator, () =>
+            {
+                ApplyEffectToObject(DurationType.Instant, EffectDamage(damage, DamageType.Fire), target);
+            });
+        }
+
+        private void Flame1()
+        {
+            _builder.Create(FeatType.Flame1)
+                .Name(LocaleString.FlameI)
+                .Description(LocaleString.FlameIDescription)
+                .HasRecastDelay(RecastGroup.Flame, 2f)
+                .HasActivationDelay(1f)
+                .DisplaysVisualEffectWhenActivating()
+                .UsesAnimation(AnimationType.LoopingConjure1)
+                .IsCastedAbility()
+                .RequirementEP(6)
+                .ResonanceCost(1)
+                .HasMaxRange(10f)
+                .IsHostile()
+                .HasImpactAction((activator, target, location) =>
+                {
+                    Impact(activator, target, 10);
+                });
+        }
+
+        private void Flame2()
+        {
+            _builder.Create(FeatType.Flame2)
+                .Name(LocaleString.FlameII)
+                .Description(LocaleString.FlameIIDescription)
+                .HasRecastDelay(RecastGroup.Flame, 3f)
+                .HasActivationDelay(2f)
+                .DisplaysVisualEffectWhenActivating()
+                .UsesAnimation(AnimationType.LoopingConjure1)
+                .IsCastedAbility()
+                .RequirementEP(22)
+                .ResonanceCost(2)
+                .HasMaxRange(10f)
+                .IsHostile()
+                .HasImpactAction((activator, target, location) =>
+                {
+                    Impact(activator, target, 60);
+                });
+        }
+
+        private void Flame3()
+        {
+            _builder.Create(FeatType.Flame3)
+                .Name(LocaleString.FlameIII)
+                .Description(LocaleString.FlameIIIDescription)
+                .HasRecastDelay(RecastGroup.Flame, 4f)
+                .HasActivationDelay(3f)
+                .DisplaysVisualEffectWhenActivating()
+                .UsesAnimation(AnimationType.LoopingConjure1)
+                .IsCastedAbility()
+                .RequirementEP(65)
+                .ResonanceCost(3)
+                .HasMaxRange(10f)
+                .IsHostile()
+                .HasImpactAction((activator, target, location) =>
+                {
+                    Impact(activator, target, 120);
+                });
+        }
+    }
+}
