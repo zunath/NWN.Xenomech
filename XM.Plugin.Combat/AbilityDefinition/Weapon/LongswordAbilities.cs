621182bbc70d0c0a9e9481afe2adc5656e1486b4
Zunath <coolty3001@yahoo.com>
2025-02-24 22:30:31 -0500

    Add service binding

--- XM.Plugin.Combat/AbilityDefinition/Weapon/LongswordAbilities.cs
+++ XM.Plugin.Combat/AbilityDefinition/Weapon/LongswordAbilities.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Anvil.Services;
 using XM.Plugin.Combat.StatusEffectDefinition.WeaponSkill;
 using XM.Progression.Ability;
 using XM.Progression.Skill;
@@ -7,6 +8,7 @@ using XM.Shared.Core.Localization;
 
 namespace XM.Plugin.Combat.AbilityDefinition.Weapon
 {
+    [ServiceBinding(typeof(IAbilityListDefinition))]
     internal class LongswordAbilities : IAbilityListDefinition
     {
         private readonly AbilityBuilder _builder = new();
