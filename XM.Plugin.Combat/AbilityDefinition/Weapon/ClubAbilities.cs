c7b3ce3eed244fe597b8e4e3da1e95c32562ec74
Zunath <coolty3001@yahoo.com>
2025-03-01 14:30:50 -0500

    Flash Nova and Judgment

--- XM.Plugin.Combat/AbilityDefinition/Weapon/ClubAbilities.cs
+++ XM.Plugin.Combat/AbilityDefinition/Weapon/ClubAbilities.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Anvil.Services;
+using XM.Plugin.Combat.StatusEffectDefinition.Debuff;
 using XM.Plugin.Combat.StatusEffectDefinition.WeaponSkill;
 using XM.Progression.Ability;
 using XM.Progression.Skill;
@@ -15,13 +16,22 @@ namespace XM.Plugin.Combat.AbilityDefinition.Weapon
     internal class ClubAbilities : WeaponSkillBaseAbility
     {
         private readonly AbilityBuilder _builder = new();
+        private readonly Lazy<StatusEffectService> _status;
+        private readonly SpellService _spell;
+        private readonly StatService _stat;
 
         public ClubAbilities(
             Lazy<CombatService> combat,
-            Lazy<StatusEffectService> status)
+            Lazy<StatusEffectService> status,
+            SpellService spell,
+            StatService stat)
             : base(combat, status)
         {
+            _status = status;
+            _spell = spell;
+            _stat = stat;
         }
+
         public override Dictionary<FeatType, AbilityDetail> BuildAbilities()
         {
             ShiningStrike();
@@ -113,12 +123,49 @@ namespace XM.Plugin.Combat.AbilityDefinition.Weapon
 
         private void FlashNova()
         {
-
+            _builder.Create(FeatType.FlashNova)
+                .Name(LocaleString.FlashNova)
+                .Description(LocaleString.FlashNovaDescription)
+                .IsWeaponSkill(SkillType.Club, 1130)
+                .RequirementTP(1350)
+                .TelegraphSize(4f, 2f)
+                .HasTelegraphConeAction((activator, targets, location) =>
+                {
+                    foreach (var target in targets)
+                    {
+                        var duration = _spell.CalculateResistedTicks(target, ResistType.Light, 6);
+                        _status.Value.ApplyStatusEffect<BlindStatusEffect>(activator, target, duration);
+                    }
+                });
         }
 
         private void Judgment()
         {
+            _builder.Create(FeatType.Judgment)
+                .Name(LocaleString.Judgment)
+                .Description(LocaleString.JudgmentDescription)
+                .IsWeaponSkill(SkillType.Club, 1390)
+                .RequirementTP(2000)
+                .HasImpactAction((activator, target, location) =>
+                {
+                    var maxHP = _stat.GetMaxHP(activator);
+                    var healing = (int)(maxHP * 0.4f);
 
+                    AssignCommand(activator, () =>
+                    {
+                        ApplyEffectToObject(DurationType.Instant, EffectHeal(healing), activator);
+                    });
+
+                    ApplyEffectToObject(DurationType.Instant, EffectVisualEffect(VisualEffectType.ImpHealingExtra), activator);
+                })
+                .HasTelegraphConeAction((activator, targets, location) =>
+                {
+                    foreach (var target in targets)
+                    {
+                        var duration = _spell.CalculateResistedTicks(target, ResistType.Light, 6);
+                        _status.Value.ApplyStatusEffect<BlindStatusEffect>(activator, target, duration);
+                    }
+                });
         }
 
         private void HexaStrike()
