404e9b3697850e6b7d7ecdb954e66bd71127255a
Zunath <coolty3001@yahoo.com>
2025-08-09 12:38:57 -0400

    Add way to select durability points via the repair UI. Also clean up unnecessary conversation chat command.

--- XM.Shared.Inventory/Durability/UI/ItemRepairView.cs
+++ XM.Shared.Inventory/Durability/UI/ItemRepairView.cs
@@ -26,7 +26,7 @@ namespace XM.Inventory.Durability.UI
                     .IsClosable(true)
                     .IsTransparent(false)
                     .Title(LocaleString.RepairItem)
-                    .InitialGeometry(0, 0, 330, 330)
+                    .InitialGeometry(0, 0, 360, 400)
                     .Root(root =>
                     {
                         root.AddRow(row =>
@@ -51,8 +51,56 @@ namespace XM.Inventory.Durability.UI
                                 button
                                     .ResRef(model => model.ItemIconResref)
                                     .Width(64f)
-                                    .Height(64f)
+                                    .Height(128f)
                                     .OnClick(model => model.OnSelectItem());
+                            });
+                            row.AddSpacer();
+                        });
+
+                        // Repair amount selector
+                        root.AddRow(row =>
+                        {
+                            row.AddSpacer();
+                            row.AddLabel(label =>
+                            {
+                                label
+                                    .HorizontalAlign(NuiHAlign.Right)
+                                    .VerticalAlign(NuiVAlign.Middle)
+                                    .Label(LocaleString.RepairAmount)
+                                    .Width(110f)
+                                    .Height(26f);
+                            });
+                            row.AddSlider(slider =>
+                            {
+                                slider
+                                    .Value(model => model.SelectedRepairPoints)
+                                    .Min(model => model.MinRepairPoints)
+                                    .Max(model => model.MaxRepairPoints)
+                                    .Step(1);
+                            });
+                            row.AddLabel(label =>
+                            {
+                                label
+                                    .HorizontalAlign(NuiHAlign.Left)
+                                    .VerticalAlign(NuiVAlign.Middle)
+                                    .Label(model => model.SelectedRepairPoints)
+                                    .Width(50f)
+                                    .Height(26f);
+                            });
+                            row.AddSpacer();
+                        });
+
+                        // Price display
+                        root.AddRow(row =>
+                        {
+                            row.AddSpacer();
+                            row.AddLabel(label =>
+                            {
+                                label
+                                    .HorizontalAlign(NuiHAlign.Center)
+                                    .VerticalAlign(NuiVAlign.Middle)
+                                    .Label(model => model.Price)
+                                    .Height(26f);
                             });
                             row.AddSpacer();
                         });
